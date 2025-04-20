c6b1115c09e910a00e4aac473e6dc185a8a04e0a
Sangam Gupta <sangamg7318@gmail.com>
2025-04-21 00:51:14 +0530

    Update order status

--- Server/src/models/OrderModel.js
+++ Server/src/models/OrderModel.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const OrderSchema=new mongoose.Schema({
   user:{type:mongoose.Schema.Types.ObjectId, ref:'User'},
   bottleQuantity:{type:Number, default:1},
-  status:{type:String, enum:['pending', 'assigned', 'delivered'], default:'pending'}
+  status:{type:String, enum:['Pending', 'Assigned', 'Delivered'], default:'Pending'}
 }, {timestamps:true});
 
 
